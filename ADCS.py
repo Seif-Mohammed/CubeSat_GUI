20a7e64533b8d3004209142f5befd5a157a53ea5
Seif Mohamed <seifmohamed606@gmail.com>
2024-02-02 19:20:26 +0200

    Update ADCS.py

--- ADCS.py
+++ ADCS.py
@@ -1,10 +1,12 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QLabel, QLineEdit
+from PyQt6 import QtWidgets , QtCore
+from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QLabel, QLineEdit , QTabWidget
 from PyQt6.QtGui import QFont, QColor ,QPixmap
 
 class SensorApp(QWidget):
     def __init__(self):
         super().__init__()
+        
         self.setWindowTitle("Sensor Data GUI")
         self.setGeometry(100, 100, 700, 300)
         background_label = QLabel(self)
@@ -15,12 +17,27 @@ class SensorApp(QWidget):
         # Set a lower stacking order for the background_label
         background_label.lower()
 
+        
 
         self.setup_ui()
 
     def setup_ui(self):
         main_layout = QVBoxLayout(self)
-
+        self.tab_widget = QtWidgets.QTabWidget(self)
+        self.tab_widget.setMaximumSize(QtCore.QSize(16777215, 50))
+        self.tab_widget.setStyleSheet("font: 10pt \"MS Shell Dlg 2\";\n"
+"font: 20pt \"MS Shell Dlg 2\";")
+        self.tab_widget.setIconSize(QtCore.QSize(40, 18))
+        self.tab_widget.setObjectName("tab_widget")
+        # Create tabs and their corresponding widgets
+        tabs = ["MainWindow", "COMM", "EPS", "ADCS", "OBC"]
+        for tab_name in tabs:
+            self.tab = QWidget()
+            self.tab_widget.addTab(self.tab , tab_name)
+        self.tab_widget.setCurrentIndex(0)
+        main_layout.addWidget(self.tab_widget)
+        
+        self.setLayout(main_layout)
         categories = [
             ("LDRs", ["Lux 1", "Lux 2", "Lux 3", "Lux 4"]),
             ("Accelerometer", ["Acc X", "Acc Y", "Acc Z"]),
@@ -66,6 +83,7 @@ class SensorApp(QWidget):
                 row_layout.addWidget(category_group_box)
 
             main_layout.addLayout(row_layout)
+
 
         self.setLayout(main_layout)
 
